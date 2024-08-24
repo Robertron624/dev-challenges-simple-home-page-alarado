06d08582f373ce64282b41016223b14a8e2853e9
Robertron624 <robert.624@hotmail.com>
2024-08-24 15:22:36 -0500

    feat: first implementation of theme switcher with javascript

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
-// Add some interactivity to the website
-console.log("Hello, world!");
+function main () {
+
+    const logoImg = document.querySelector('#logo-img');
+    const themeToggle = document.querySelector('#theme-toggle');
+
+    themeToggle.addEventListener('change', function () {
+
+        const isDarkMode = this.checked;
+
+        if (isDarkMode) {
+            document.body.classList.add('dark');
+            logoImg.src = 'assets/images/alarado-icon-homepage-dark.svg';
+        } else {
+            document.body.classList.remove('dark');
+            logoImg.src = 'assets/images/alarado-icon-homepage.svg';
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', main);
