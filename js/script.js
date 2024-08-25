f896fc7c556abfdc96deb8f3eae9fd8f40397c3f
Robertron624 <robert.624@hotmail.com>
2024-08-25 12:22:57 -0500

    style: Update theme switcher styles to resemble more to the design

--- js/script.js
+++ js/script.js
@@ -1,19 +1,23 @@
 function main () {
-
+    const body = document.body;
     const logoImg = document.querySelector('#logo-img');
+
+    function applyTheme(isDark) {
+        if (isDark) {
+            body.classList.add('dark');
+            logoImg.src = 'assets/images/alarado-icon-homepage-dark.svg';
+        } else {
+            body.classList.remove('dark');
+            logoImg.src = 'assets/images/alarado-icon-homepage.svg';
+        }
+    }
+
+    
     const themeToggle = document.querySelector('#theme-toggle');
 
     themeToggle.addEventListener('change', function () {
-
         const isDarkMode = this.checked;
-
-        if (isDarkMode) {
-            document.body.classList.add('dark');
-            logoImg.src = 'assets/images/alarado-icon-homepage-dark.svg';
-        } else {
-            document.body.classList.remove('dark');
-            logoImg.src = 'assets/images/alarado-icon-homepage.svg';
-        }
+        applyTheme(isDarkMode);
     });
 }
 
