106a11f5883b4ee03c9d865d50bdeeb9cf6db86f
Roberto Ramirez <robert.624@hotmail.com>
2024-08-27 09:30:33 -0500

    feat: adding theme switcher to mobile menu

--- js/script.js
+++ js/script.js
@@ -1,57 +1,74 @@
-function main () {
-    const body = document.body;
-    const logoImg = document.querySelector('#logo-img');
+function main() {
+  const body = document.body;
+  const logoImg = document.querySelector("#logo-img");
 
-    function applyTheme(isDark) {
-        if (isDark) {
-            body.classList.add('dark');
-            logoImg.src = 'assets/images/alarado-icon-homepage-dark.svg';
-        } else {
-            body.classList.remove('dark');
-            logoImg.src = 'assets/images/alarado-icon-homepage.svg';
+  function applyTheme(isDark) {
+    if (isDark) {
+      body.classList.add("dark");
+      logoImg.src = "assets/images/alarado-icon-homepage-dark.svg";
+    } else {
+      body.classList.remove("dark");
+      logoImg.src = "assets/images/alarado-icon-homepage.svg";
+    }
+  }
+
+  // By default, take the user's system preference
+  const prefersDarkMode = window.matchMedia(
+    "(prefers-color-scheme: dark)"
+  ).matches;
+  applyTheme(prefersDarkMode);
+
+  // Listen for changes to the theme toggle
+  const themeToggles = document.querySelectorAll(
+    "#theme-toggle",
+    "#theme-toggle-mobile"
+  );
+
+  // themeToggle.addEventListener('change', function () {
+  //     const isDarkMode = this.checked;
+  //     applyTheme(isDarkMode);
+  // });
+
+  themeToggles.forEach((toggle) => {
+    toggle.checked = prefersDarkMode;
+
+    toggle.addEventListener("change", function () {
+      const isDarkMode = this.checked;
+      applyTheme(isDarkMode);
+
+      // Sync both toggles
+      themeToggles.forEach((t) => {
+        if (t !== this) {
+          t.checked = isDarkMode;
         }
-    }
-
-    // By default, take the user's system preference
-    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    applyTheme(prefersDarkMode);
-
-
-    // Listen for changes to the theme toggle
-    const themeToggle = document.querySelector('#theme-toggle');
-
-    themeToggle.addEventListener('change', function () {
-        const isDarkMode = this.checked;
-        applyTheme(isDarkMode);
+      });
     });
+  });
 
+  // Handle the mobile menu
 
-    // Handle the mobile menu
+  const mobileHamburguerButton = document.querySelector("#mobile-hamburger");
+  const mobileMenu = document.querySelector("#mobile-menu");
+  const closeMenuBtn = document.querySelector("#close-menu");
+  const overlay = document.querySelector("#overlay");
 
-    const mobileHamburguerButton = document.querySelector('#mobile-hamburger');
-    const mobileMenu = document.querySelector("#mobile-menu");
-    const closeMenuBtn = document.querySelector("#close-menu");
-    const overlay = document.querySelector("#overlay");
+  function openMenu() {
+    mobileMenu.classList.add("show-menu");
+    overlay.classList.add("show-overlay");
+    body.style.overflow = "hidden"; // Prevent scrolling
+  }
 
-    function openMenu() {
-        mobileMenu.classList.add('show-menu');
-        overlay.classList.add('show-overlay');
-        body.style.overflow = 'hidden'; // Prevent scrolling
-    }
-    
-    function closeMenu() {
-        mobileMenu.classList.remove('show-menu');
-        overlay.classList.remove('show-overlay');
-        body.style.overflow = 'auto'; // Allow scrolling
-    }
+  function closeMenu() {
+    mobileMenu.classList.remove("show-menu");
+    overlay.classList.remove("show-overlay");
+    body.style.overflow = "auto"; // Allow scrolling
+  }
 
-    mobileHamburguerButton.addEventListener('click', openMenu);
-    closeMenuBtn.addEventListener('click', closeMenu);
+  mobileHamburguerButton.addEventListener("click", openMenu);
+  closeMenuBtn.addEventListener("click", closeMenu);
 
-    // Close the menu when clicking on the overlay
-    overlay.addEventListener('click', closeMenu);
-
-
+  // Close the menu when clicking on the overlay
+  overlay.addEventListener("click", closeMenu);
 }
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener("DOMContentLoaded", main);
